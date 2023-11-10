98401e76642a7f45897abfdbb969c6d320c4d52e
MassimoPeruga <spamma-mutevole0p@icloud.com>
2023-11-10 19:39:19 +0100

    feat: MILESTONE 2

--- js/main.js
+++ js/main.js
@@ -2,6 +2,9 @@
 
 const images = ["./img/01.jpg", "./img/02.jpg", "./img/03.jpg", "./img/04.jpg", "./img/05.jpg"];
 const itemsContainer = document.querySelector(".items");
+const prev = document.querySelector('.prev');
+const next = document.querySelector('.next');
+
 let currentImage = 0;
 
 for (let i = 0; i < images.length; i++) {
@@ -20,3 +23,29 @@ for (let i = 0; i < images.length; i++) {
     item.appendChild(img);
     itemsContainer.appendChild(item);
 }
+
+const allItems = document.querySelectorAll(".item");
+
+prev.addEventListener('click', function () {
+
+    allItems[currentImage].classList.remove('active');
+
+    if (currentImage > 0) {
+        currentImage--;
+    }
+
+    allItems[currentImage].classList.add('active');
+
+})
+
+next.addEventListener('click', function () {
+
+    allItems[currentImage].classList.remove('active');
+
+    if (currentImage < images.length - 1) {
+        currentImage++;
+    }
+
+    allItems[currentImage].classList.add('active');
+
+})
